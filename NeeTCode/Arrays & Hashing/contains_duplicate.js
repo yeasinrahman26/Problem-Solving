e9901ce27541f34ecf7f3e110b645176223a61e9
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2026-06-30 17:07:41 +0600

    Implement twoSum function and add test case in index.js

--- NeeTCode/Arrays & Hashing/contains_duplicate.js
+++ NeeTCode/Arrays & Hashing/contains_duplicate.js
@@ -1,13 +1,13 @@
-// function twoSum(num, target) {
-//   for (let i = 0; i < num.length; i++) {
-//     for (let j = i + 1; j < num.length; j++) {
-//       if (num[i] + num[j] === target) {
-//         return [i, j];
-//       }
-//     }
-//   }
-// }
-// console.log(twoSum([2, 7, 11, 15], 9));
+function twoSum(num, target) {
+  for (let i = 0; i < num.length; i++) {
+    for (let j = i + 1; j < num.length; j++) {
+      if (num[i] + num[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+console.log(twoSum([2, 7, 11, 15], 9));
 
 // function array(arr) {
 //   let sum = 0;
---
+++ NeeTCode/Two Sum/index.js
@@ -0,0 +1,11 @@
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([3, 4, 5, 6], 7));
